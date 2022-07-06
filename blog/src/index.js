2d3d11bb139385f7d8a20e2f8946043fe9f91b2c
Nenad Skrbic <nenad.skrbic1917@gmail.com>
2022-07-06 15:47:50 +0200

    mini blog project

---
+++ blog/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
